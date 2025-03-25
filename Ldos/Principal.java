a49057ecf6766fc4fcabb32fdf5c21d54a6cb658
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-25 16:46:10 -0600

    Clase Principal

---
+++ Ldos/Principal.java
@@ -0,0 +1,19 @@
+package Ldos;
+
+public class Principal {
+    public static void main(String []args){
+        Bolsa < Chocolatina > bolsaCho = new Bolsa < Chocolatina > ();
+        
+        Chocolatina c = new Chocolatina("milka");
+        Chocolatina c1 = new Chocolatina("milka");
+        Chocolatina c2 = new Chocolatina("ferrero");
+
+        bolsaCho.add(c);
+        bolsaCho.add(c1);
+        bolsaCho.add(c2);
+
+        for (Chocolatina chocolatina: bolsaCho){
+            System.out.println(chocolatina.getMarca());
+        }
+    }
+}
