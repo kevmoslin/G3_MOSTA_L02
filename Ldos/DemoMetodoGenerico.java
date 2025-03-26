eb6a27a81695948c2e6b2be690a4861aa8eda014
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-26 12:28:41 -0600

    MetodoGenerico

---
+++ Ldos/DemoMetodoGenerico.java
@@ -0,0 +1,7 @@
+package Ldos;
+
+public class DemoMetodoGenerico {
+    static < T extends Comparable <T>> boolean igualArrays (T[] x, T[] y){
+        //Si las longitudes de los arrays son diferentes
+    }
+}
