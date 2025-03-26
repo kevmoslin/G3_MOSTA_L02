28f6bf73814c604802b15f16e34d7fcdac1419f5
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-26 12:54:50 -0600

    metodo main de DemoMetodoGenerico

--- Ldos/DemoMetodoGenerico.java
+++ Ldos/DemoMetodoGenerico.java
@@ -2,6 +2,39 @@ package Ldos;
 
 public class DemoMetodoGenerico {
     static < T extends Comparable <T>> boolean igualArrays (T[] x, T[] y){
-        //Si las longitudes de los arrays son diferentes
+        //Si las longitudes de los arrays son diferentes,
+        //entonces los arrays son diferentes.
+        if (x.length != y.length) {
+            return false;
+        }
+        for (int i = 0; i < x.length; i++){
+            if (!x[i].equals(y[i])) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    public static void main(String []args){
+        Integer nums[] = {1,2,3,4,5};
+        Integer nums2[] = {1,2,3,4,5};
+        Integer nums3[] = {1,2,3,4,5};
+        Integer nums4[] = {1,2,3,4,5};
+
+        if (igualArrays(nums, nums)) {
+            System.out.println("nums es igual a nums");
+        }
+        if (igualArrays(nums, nums2)) {
+            System.out.println("nums es igual a nums2");
+        }
+        if (igualArrays(nums, nums3)) {
+            System.out.println("nums es igual a nums3");
+        }
+        if (igualArrays(nums, nums4)) {
+            System.out.println("nums es igual a nums4");
+        }
+
+        //crea un array de double 
+    }
+
 }
