582df847c40555606451f64a7c429aaeca815de8
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-27 11:31:00 -0500

    duplicando codigos

---
+++ Ldos_ejercicios/Bolsa.java
@@ -0,0 +1,29 @@
+package Ldos_ejercicios;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa < T > implements Iterable < T > {
+    private ArrayList < T > lista;
+    private int tope;
+
+    public Bolsa() {
+        this.lista = new ArrayList<>();
+        this.tope = 10;  
+    }
+    
+    public Bolsa(int tope) {
+        this.tope = tope;  
+        this.lista = new ArrayList<>();
+    }
+
+    public void add(T objeto){
+        if (lista.size() < tope) {
+            lista.add(objeto);
+        } else {
+            throw new RuntimeException("no caben mas");
+        }
+    }
+    public Iterator < T > iterator(){
+        return lista.iterator();
+    }
+}
--- Ldos_ejercicios/DemoMetodoGenerico.java
+++ Ldos_ejercicios/DemoMetodoGenerico.java
@@ -23,38 +23,4 @@ public class DemoMetodoGenerico {
         }
         return false;
     }
-
-    public static void main(String []args){
-        Integer nums[] = {1,2,3,4,5};
-        Integer nums2[] = {1,2,3,4,5};
-        Integer nums3[] = {1,2,3,4,5};
-        Integer nums4[] = {1,2,3,4,5};
-
-        String[] v = {"Perez", "Sanchez", "Rodriguez"};
-        Integer[] w = {12, 34, 56};
-
-        System.out.println(exist(v, "Sanchez"));
-        System.out.println(exist(w, 34));
-        System.out.println(exist(w,"Salas"));
-
-        if (igualArrays(nums, nums)) {
-            System.out.println("nums es igual a nums");
-        }
-        if (igualArrays(nums, nums2)) {
-            System.out.println("nums es igual a nums2");
-        }
-        if (igualArrays(nums, nums3)) {
-            System.out.println("nums es igual a nums3");
-        }
-        if (igualArrays(nums, nums4)) {
-            System.out.println("nums es igual a nums4");
-        }
-
-        /*crea un array de double                   //A
-        Double dvals[] = {1.1,2.2,3.3,4.4,5.5};   //B
-        if (igualArrays(nums, dvals)) {
-            System.out.println("nums es igual a dvals");
-        }   */            
-    }
-
 }
