3a90cf1b40e1ee4aaab405a6434730a0933c9236
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-27 09:59:48 -0500

    intentando arrreglar error de principal

--- Ldos/Bolsa.java
+++ Ldos/Bolsa.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Bolsa < T > implements Iterable < T > {
-    private ArrayList < T > lista = new ArrayList<T>();
+    private ArrayList < T > lista;
     private int tope;
 
+    public Bolsa(){
+        this.lista = new ArrayList<>();
+    }
+    
     public Bolsa(int tope) {
         super();
         this.tope = tope;
