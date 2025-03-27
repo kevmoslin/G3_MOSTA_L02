752f8fc45636186185252a99336f4ebb064c0621
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-27 17:32:01 -0500

    clase caja

--- Ldos_ejercicios/caja.java
+++ Ldos_ejercicios/caja.java
@@ -4,5 +4,24 @@ public class caja<T> {
     private String color;
     private T contenido;
 
-    
+    public caja(String color, T contenido) {
+        this.color = color;
+        this.contenido = contenido;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public T getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(T contenido) {
+        this.contenido = contenido;
+    }
+
+    public String toString() {
+        return "Caja de color " + color + " contiene: " + contenido.toString();
+    }
 }
