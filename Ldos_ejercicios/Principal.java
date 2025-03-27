9a9ce3e2b2b620860cb587d359084db37d1835c2
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-27 14:34:40 -0500

    duplicando principal

---
+++ Ldos_ejercicios/Principal.java
@@ -0,0 +1,31 @@
+package Ldos_ejercicios;
+
+public class Principal {
+    public static void main(String []args){
+        Bolsa < Chocolatina > bolsaCho = new Bolsa < Chocolatina > ();
+        Bolsa < Golosina > bolsaGo = new Bolsa < Golosina > ();
+
+        Golosina g = new Golosina("tuyo", 5);
+        Golosina g2 = new Golosina("ole ole", 4);
+        
+        Chocolatina c = new Chocolatina("milka");
+        Chocolatina c1 = new Chocolatina("milka");
+        Chocolatina c2 = new Chocolatina("ferrero");
+
+        bolsaGo.add(g);
+        bolsaGo.add(g2);
+        
+        bolsaCho.add(c);
+        bolsaCho.add(c1);
+        bolsaCho.add(c2);
+
+        for (Chocolatina chocolatina: bolsaCho){
+            System.out.println(chocolatina.getMarca());
+        }
+
+        for (Golosina golosina: bolsaGo){
+            System.out.println(golosina.getNombre());
+            System.out.println(golosina.getPeso());
+        }
+    }
+}
